8edc93ffd16ea960106639a5b66f569aaae84d81
Varun <varunsridharan23@gmail.com>
2020-04-29 11:02:12 +0530

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# $1 - Envato Username
-# $2 - Envato Personal Token
-# $3 - Custom Command
-# $4 - Exclude Files
 set -eu
 
 ENVATO_USERNAME="${INPUT_ENVATO_USERNAME}"
@@ -15,11 +11,10 @@ ASSETS_EXCLUDE_LIST="${INPUT_ASSETS_EXCLUDE_LIST}"
 # Allow some ENV variables to be customized
 SLUG=${GITHUB_REPOSITORY#*/}
 
-
 # Set VERSION value according to tag value.
 VERSION=${GITHUB_REF#refs/tags/}
 
-if [[ $VERSION = $GITHUB_REF ]]; then
+if [[ $VERSION == $GITHUB_REF ]]; then
   VERSION=${GITHUB_REF#refs/heads/}
 fi
 
@@ -32,16 +27,16 @@ fi
 # Files That Are Needed To Be Excluded
 if [[ ! -z "$EXCLUDE_LIST" ]]; then
   echo "Saving Excluded File List"
-  echo $EXCLUDE_LIST | tr " " "\n" >> envato_exclude_list.txt
+  echo $EXCLUDE_LIST | tr " " "\n" >>envato_exclude_list.txt
 fi
 # Files That Are Needed To Be Excluded
 if [[ ! -z "$ASSETS_EXCLUDE_LIST" ]]; then
   echo "Saving Excluded File List"
-  echo $ASSETS_EXCLUDE_LIST | tr " " "\n" >> envato_assets_exclude_list.txt
+  echo $ASSETS_EXCLUDE_LIST | tr " " "\n" >>envato_assets_exclude_list.txt
 fi
 
-echo ".git .github exclude.txt node_modules envato_exclude_list.txt .gitattributes .gitignore .DS_Store" | tr " " "\n" >> envato_exclude_list.txt
-echo "screenshots/ *.psd .DS_Store Thumbs.db ehthumbs.db ehthumbs_vista.db .git .github .gitignore .gitattributes node_modules" | tr " " "\n" >> envato_assets_exclude_list.txt
+echo ".git .github exclude.txt node_modules envato_exclude_list.txt .gitattributes .gitignore .DS_Store" | tr " " "\n" >>envato_exclude_list.txt
+echo "screenshots/ *.psd .DS_Store Thumbs.db ehthumbs.db ehthumbs_vista.db .git .github .gitignore .gitattributes node_modules" | tr " " "\n" >>envato_assets_exclude_list.txt
 
 echo "Creating Required Temp Dirs"
 mkdir ../envato-draft-source/
@@ -56,7 +51,6 @@ rsync -r --delete --exclude-from="./envato_exclude_list.txt" "./" ../envato-draf
 echo "Copying Banner, Icon & Screenshots"
 rsync -r --delete --exclude-from="./envato_assets_exclude_list.txt" "./$ASSETS_PATH/" ../envato-draft-source-assets
 rsync -r --delete --exclude-from="./envato_assets_exclude_list.txt" "./$ASSETS_PATH/screenshots/" ../envato-draft-source-screenshots
-
 
 echo "Generating Final Zip File"
 cd ../envato-draft-source/
