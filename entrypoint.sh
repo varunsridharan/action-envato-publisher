03cf5d9a9c08d244084444e16ef7260ce02f9ba4
Varun <varunsridharan23@gmail.com>
2020-06-27 13:08:43 +0530

    Force Ignore Defaults

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,6 @@ if [ ! -f "$GITHUB_WORKSPACE/$DIST_IGNORE" ]; then
   echo "⚠️ Dist Ignore File Not Found !"
   DIST_IGNORE_PATH="${GITHUB_WORKSPACE}/envato_distignore.txt"
   touch $DIST_IGNORE_PATH
-  echo ".git .github $ASSETS_IGNORE node_modules $DIST_IGNORE .gitattributes .gitignore .DS_Store" | tr " " "\n" >>"$DIST_IGNORE_PATH"
 elif [ -f "$DIST_IGNORE" ]; then
   DIST_IGNORE_PATH="$GITHUB_WORKSPACE/$DIST_IGNORE"
 fi
@@ -33,9 +32,16 @@ if [ ! -f "$GITHUB_WORKSPACE/$ASSETS_IGNORE" ]; then
   echo "⚠️ Assets Ignore File Not Found !"
   ASSETS_IGNORE_PATH="${GITHUB_WORKSPACE}/envato_assets_distignore.txt"
   touch $ASSETS_IGNORE_PATH
-  echo "screenshots/ *.psd .DS_Store *.db .git .github .gitignore .gitattributes node_modules" | tr " " "\n" >>"$ASSETS_IGNORE_PATH"
 elif [ -f "$ASSETS_IGNORE" ]; then
   ASSETS_IGNORE_PATH="$GITHUB_WORKSPACE/$ASSETS_IGNORE"
+fi
+
+if [ ! -z "$ASSETS_IGNORE_PATH"]; then
+  echo "screenshots/ *.psd .DS_Store *.db .git .github .gitignore .gitattributes node_modules" | tr " " "\n" >>"$ASSETS_IGNORE_PATH"
+fi
+
+if [ ! -z "$DIST_IGNORE_PATH"]; then
+  echo "$ASSETS_IGNORE $DIST_IGNORE .git .github node_modules .gitattributes .gitignore .DS_Store" | tr " " "\n" >>"$DIST_IGNORE_PATH"
 fi
 
 echo "✅ Creating Required Temp Directories"
